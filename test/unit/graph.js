3a849b78ca520d9237bcb0e393733b56ad2148f9
jetiny <86287344@qq.com>
2017-05-23 16:33:45 +0800

    update test graph

--- test/unit/graph.js
+++ test/unit/graph.js
@@ -5,7 +5,7 @@ import ApiArticle from './fixtures/interface/api/Article.js'
 import LayoutUser from './fixtures/interface/layout/User.js'
 import PageHome from './fixtures/interface/page/Home.js'
 import PageArticle from './fixtures/interface/page/Article.js'
-import * as SchemaUser from './fixtures/interface/schema/User.js'
+import SchemaUser from './fixtures/interface/schema/User.js'
 
 import {Graph} from 'sav'
 
@@ -27,5 +27,5 @@ test('graph & decorator', (ava) => {
   expect(JSON.stringify(graph.layout.User)).to.eql(JSON.stringify(LayoutUser))
   expect(JSON.stringify(graph.page.Home)).to.eql(JSON.stringify(PageHome))
   expect(JSON.stringify(graph.page.Article)).to.eql(JSON.stringify(PageArticle))
-  expect(JSON.stringify(graph.schema)).to.eql(JSON.stringify(SchemaUser.default))
+  expect(JSON.stringify(graph.schema)).to.eql(JSON.stringify(SchemaUser))
 })
