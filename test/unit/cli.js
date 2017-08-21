aa615dca8b960ce47f26b3a754be7e698ce3f22f
jetiny <86287344@qq.com>
2017-08-22 07:55:00 +0800

    add sassWriter.js

--- test/unit/cli.js
+++ test/unit/cli.js
@@ -1,17 +1,20 @@
 import test from 'ava'
 import {expect} from 'chai'
-import {loadInterface, writeContract, writeActions} from 'sav-cli'
+import {loadInterface, writeContract, writeActions, writeSass} from 'sav-cli'
 import path from 'path'
 
 test('api', async (ava) => {
   expect(loadInterface).to.be.a('function')
   expect(writeContract).to.be.a('Function')
   expect(writeActions).to.be.a('Function')
+  expect(writeSass).to.be.a('Function')
 })
 
 test('convertInterface', async (ava) => {
   let modules = await loadInterface(path.resolve(__dirname, './fixtures/interface'))
   await writeContract(path.resolve(__dirname, './fixtures/contract'), modules)
   await writeActions(path.resolve(__dirname, './fixtures/actions'), modules.actions)
+  await writeSass(path.resolve(__dirname, './fixtures/sass-page'), modules.actions, {pageMode: true})
+  await writeSass(path.resolve(__dirname, './fixtures/sass-view'), modules.actions, {pageMode: false})
   expect(modules).to.be.a('object')
 })
