029ce168c22565bd9b09505e62a9beebd1243bf2
jetiny <86287344@qq.com>
2017-09-26 07:49:53 +0800

    remove src

--- src/sav-cli/applyUri.js
+++
@@ -1,5 +0,0 @@
-import {normalizeUris} from '../sav/plugins/uri.js'
-
-export function applyUri (groups) {
-  normalizeUris(groups)
-}
--- src/sav/core/index.js
+++
@@ -1,4 +0,0 @@
-export * from './exception.js'
-export * from './config.js'
-export * from './decorator'
-export * from './sav.js'
