6e0f6b9b49d49387c964af89d1f6449bb8d57f82
jetiny <86287344@qq.com>
2017-09-26 09:55:10 +0800

    add phpActionWriter.js

--- test/unit/cli.js
+++ test/unit/cli.js
@@ -1,6 +1,15 @@
 import test from 'ava'
 import {expect} from 'chai'
-import {loadInterface, writeContract, writeActions, writeSass, writeVue, writeRollup, writePackage} from 'sav-cli'
+import {
+  loadInterface,
+  writeContract,
+  writeActions,
+  writePhpActions,
+  writeSass,
+  writeVue,
+  writeRollup,
+  writePackage
+} from 'sav-cli'
 import path from 'path'
 
 test('api', async (ava) => {
@@ -10,12 +19,16 @@ test('api', async (ava) => {
   expect(writeSass).to.be.a('Function')
   expect(writeRollup).to.be.a('Function')
   expect(writePackage).to.be.a('Function')
+  expect(writePhpActions).to.be.a('Function')
 })
 
 test('convertInterface', async (ava) => {
   let modules = await loadInterface(path.resolve(__dirname, './fixtures/interface'))
-  await writeContract(path.resolve(__dirname, './fixtures/contract'), modules)
+  await writeContract(path.resolve(__dirname, './fixtures/contract'), modules
+    // , {lang: ['php', 'js']}
+  )
   await writeActions(path.resolve(__dirname, './fixtures/actions'), modules.modals)
+  await writePhpActions(path.resolve(__dirname, './fixtures/php-actions'), modules.modals)
   await writeSass(path.resolve(__dirname, './fixtures/sass'), modules.modals)
   await writeSass(path.resolve(__dirname, './fixtures/sass-view'), modules.modals, {pageMode: false})
   await writeVue(path.resolve(__dirname, './fixtures/views'), modules.modals)
