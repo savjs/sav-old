d875944a5fefc738f19222f47f8457a81b3923c8
jetiny <86267344@qq.com>
2017-11-19 22:23:56 +0800

    update payload

--- test/unit/decorator.js
+++ test/unit/decorator.js
@@ -1,14 +1,13 @@
 import test from 'ava'
 import {expect} from 'chai'
 
-import {Modal, get, post, head, options, put, patch, del} from 'sav-cli'
+import {Modal, get, post, head, put, patch, del} from 'sav-cli'
 
 test('decorator.api', (ava) => {
   expect(Modal).to.be.a('function')
   expect(get).to.be.a('function')
   expect(post).to.be.a('function')
   expect(head).to.be.a('function')
-  expect(options).to.be.a('function')
   expect(put).to.be.a('function')
   expect(patch).to.be.a('function')
   expect(del).to.be.a('function')
